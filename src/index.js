f28b2227dd8c9352ad372f27640455f48848f1d7
LiJieSu0 <opwinopwin@gmail.com>
2022-10-05 11:50:02 -0700

    add_Components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { nanoid } from "nanoid";
+
+const DATA = [
+  { id: `todo-${nanoid()}`, name: "Eat", completed: true },
+  { id: `todo-${nanoid()}`, name: "Sleep", completed: false },
+  { id: `todo-${nanoid()}`, name: "Repeat", completed: false }
+];
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA}/>
   </React.StrictMode>
 );
 
---
+++ test.js
@@ -0,0 +1,5 @@
+let newArr=[1,2,3];
+
+let newBrr=[...newArr,4];
+
+console.log(newBrr)
